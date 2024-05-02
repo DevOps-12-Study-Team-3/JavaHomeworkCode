4e813c0c4e98689f2f33bfa49f67c1d635d09ba2
arisu0304 <arisu0304@naver.com>
2024-05-02 20:56:41 +0900

    java 숙제 수정

--- LeeChungHyun - homework/middle04/Use.java
+++ LeeChungHyun - homework/middle04/Use.java
@@ -1,10 +1,10 @@
-package chap99_homework.myself04.basic;
+package chap99_homework.myself04.middle;
 
 public class Use {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Tiger tiger = new Tiger();
+		Max3.main(null);
+		Median.main(null);
 	}
-
 }
