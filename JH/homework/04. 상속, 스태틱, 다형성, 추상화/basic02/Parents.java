31b6f34656ed3b1dae5275379b10039e2a5f5175
dev2024dev <publicdev2024@gmail.com>
2024-05-02 19:20:14 +0900

    05-02 반재형 연습문제 풀이 추가

---
+++ JH/homework/04. 상속, 스태틱, 다형성, 추상화/basic02/Parents.java
@@ -0,0 +1,9 @@
+package chap_homework.home2;
+
+public class Parents {
+	//2. 성씨, 혈액형, 키, 피부색을 속성으로 가지는 Parent 클래스를 생성하고 Parent를 상속받은 ChildA, ChildB 클래스를 만들어서 ChildA에는 이름 속성을 추가하고 ChildB에는 지문 속성을 추가하세요.
+	public String firstName;
+	public String bloodType;
+	public int tall;
+	public String skin;
+}
---
+++ JH/homework/04. 상속, 스태틱, 다형성, 추상화/basic03/publicOfficer.java
@@ -0,0 +1,23 @@
+package chap_homework.home3;
+
+public class publicOfficer extends Employee {
+
+	@Override
+	public void goWork() {
+		// TODO Auto-generated method stub
+		System.out.println("열심히 근무지까지 간다.");
+	}
+
+	@Override
+	public void leaveWork() {
+		// TODO Auto-generated method stub
+		System.out.println("열심히 퇴근한다.");
+	}
+
+	@Override
+	public void working() {
+		// TODO Auto-generated method stub
+		System.out.println("일한다.");
+	}
+
+}
