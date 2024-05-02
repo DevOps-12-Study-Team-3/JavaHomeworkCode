b9f7ae1dcfbf82ed117575158cd879317ccab91f
ssongmingyo <alsrysong@gmail.com>
2024-05-02 19:28:58 +0900

    24.05.02 송민교

---
+++ SongMinGyo/MyHomework/_0502_basic/02/Parent.java
@@ -0,0 +1,20 @@
+package HW_0502_basic_02;
+
+public class Parent {
+	long firstName;
+	int bloodType;
+	int height;
+	String skinColor;
+	
+	public Parent() {
+		
+	}
+	
+	public Parent(long firstName, int bloodType, int height, String skinColor) {
+		this.firstName = firstName;
+		this.bloodType = bloodType;
+		this.height = height;
+		this.skinColor = skinColor;
+	}
+	
+}
