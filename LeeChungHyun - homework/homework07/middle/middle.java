010902a2a60e331aa55f1ea3b925c113fe832f6c
arisu0304 <arisu0304@naver.com>
2024-05-08 17:52:53 +0900

    240508 자바 숙제 수정

--- LeeChungHyun - homework/homework07/middle/middle.java
+++ LeeChungHyun - homework/homework07/middle/middle.java
@@ -9,25 +9,25 @@ public class middle {
 //		1. 크기가 30인 StringBuffer에 "hello"라는 문자열이 담겨있다.
 //	    비어있는 바이트의 공간을 b, i, t가 반복되어 들어가도록 모두 채우세요.
 		
-		StringBuffer sb = new StringBuffer(30);
-		sb.append("hello");
+		StringBuffer sb3 = new StringBuffer(30);
+		sb3.append("hello");
 		
-		int spaceNum = sb.capacity() - sb.length(); // 공백의 개수
+		int spaceNum2 = sb3.capacity() - sb3.length();
 		
-		for(int i = 0 ; i < (spaceNum/3)+1 ; i++) {
-			sb.append("bit"); // 공백에 bit 문자열 반복해서 추가
+		for(int i = 0 ; i < spaceNum2 ; i++) {
+			if( i % 3 == 0) {
+				sb3.append('b');
+			}else if( i % 3 == 1) {
+				sb3.append('i');
+			}else if(i % 3 == 2) {
+				sb3.append('t');
+			}
 		}
-		sb.setLength(30); // 문자열 길이가 30을 초과하므로 30만큼 자름
 		
-		System.out.println(sb);
+		System.out.println(sb3.capacity());
+		System.out.println(sb3.toString());
 		
 		System.out.println("----------------------");
-		
-		// 이 방식은 한번 문자열아 크기 30을 한번 초과하므로 버퍼가 자동으로 크기가 늘어나있음
-		// 여기선 62로 증가
-		// setLength는 길이만 줄이고 크기는 고정
-		// 크기도 30으로 맞추고 싶으면 bit를 mod3으로 분기시켜서 하나씩 넣어서 문자열 길이가
-		// 30이 되는 순간 for문 종료시키면 됨
 		
 //
 //		2. "유용한 클래스는 많다. 다양한 라이브러리를 활용한다."라는 StringBuffer이 있을 때 
