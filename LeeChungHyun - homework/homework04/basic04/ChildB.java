9dbed1e8a9a840398800b00c98b257c2b164bf36
arisu0304 <arisu0304@naver.com>
2024-05-09 21:23:07 +0900

    240509 숙제 수정

---
+++ LeeChungHyun - homework/homework04/basic04/ChildB.java
@@ -0,0 +1,13 @@
+package chap99_homework.myself04.basic;
+
+public class ChildB extends Parent {
+	private String fingerPrint;
+
+	public String getFingerPrint() {
+		return fingerPrint;
+	}
+
+	public void setFingerPrint(String fingerPrint) {
+		this.fingerPrint = fingerPrint;
+	}
+}
