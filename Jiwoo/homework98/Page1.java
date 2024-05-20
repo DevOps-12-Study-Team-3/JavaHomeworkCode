dea672bef420fc18ca5dbc90abc8effaa676943e
namdoil23 <gwoo28@gmail.com>
2024-05-20 19:59:13 +0900

    20240519 정지우 기초문제 1번 ver2

--- Jiwoo/homework98/Page1.java
+++ Jiwoo/homework98/Page1.java
@@ -215,8 +215,6 @@ public class Page1 {
 //          }
 //          }scanner.close();
           
-        
-        
 //
 //        3. 사용자가 실수 한 개를 입력하면 정수형으로 변환해서 출력하세요.
 //		  Scanner scanner = new Scanner(System.in);
@@ -246,17 +244,292 @@ public class Page1 {
 //		System.out.println(lNum1);
 //
 //        6. 사용자가 입력한 두 개의 문자 각각의 유니코드를 출력하세요.
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("첫번째문자를 입력하세요. ");
-		String str1 = scanner.nextLine();
-		System.out.println("두번째문자를 입력하세요. ");
-		String str2 = scanner.nextLine();
-        
-		char[] cha1 = str1.toCharArray();
-		char[] cha2 = str2.toCharArray();
-		
-		System.out.println("입력하신 첫번재 문자의 유니코드:" + cha1);
-		System.out.println("입력하신 두번재 문자의 유니코드:" + cha2);
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("첫번째문자를 입력하세요. ");
+//		String str1 = scanner.nextLine();
+//		System.out.println("두번째문자를 입력하세요. ");
+//		String str2 = scanner.nextLine();
+//        
+//		char[] cha1 = str1.toCharArray();
+//		char[] cha2 = str2.toCharArray();
+//		
+//		System.out.println("입력하신 첫번재 문자의 유니코드:" + cha1);
+//		System.out.println("입력하신 두번재 문자의 유니코드:" + cha2);
 
+//		1. 사용자가 입력한 두 정수읜 덧셈, 뺄셈, 곱셈, 나눗셈, 나머지 연산의 결과를 출력하세요.
+//		  (메소드로 따로 만들지 말고 메인메소드에서 구현)
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("첫번째 정수를 입력하세요. ");
+//        int a = scanner.nextInt(); 
+//        System.out.println("두번째 정수를 입력하세요. ");
+//        int b = scanner.nextInt(); 
+//        int sum = a + b;
+//        int sub = a - b;
+//        int mul = a * b;
+//        int div = a / b;
+//        
+//        
+//        System.out.println("두 수의 + 값:" + sum);
+//        System.out.println("두 수의 - 값:" + sub);
+//        System.out.println("두 수의 * 값:" + mul);
+//        System.out.println("두 수의 / 값:" + div);
+        
+		
+//
+//		2. int형 변수 20을 증감연산자를 사용해서 다음과 같은 순서로 출력되도록 구현하세요. 21, 22, 22, 21, 20, 20, 19, 마지막 변수의 값(증감연산자 없이) : 20
+//
+//		int a = 20;			
+//		
+//			System.out.println(++a);	//21	21	
+//			System.out.println(++a);	//22	22
+//			System.out.println(a);		//22	22
+//			System.out.println(--a);	//21	21
+//			System.out.println(--a);	//20	20
+//			System.out.println(a--);
+//			System.out.println(a++);
+//		
+//			System.out.println(a);
+		
+//		3. 사용자가 입력한 정수가 2와 7의 공배수면 "2와 7의 공배수"라고 출력하고 아니면 "2와 7의 공배수가 아님"이라고 출력하세요.
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("정수를 입력하세요. ");
+//		int a = scanner.nextInt(); 
+//			if (a % 14 == 0) {
+//				System.out.println("2와 7의 공배수");
+//			}else {
+//				System.out.println("2와 7의 공배수가 아님");
+//			}	
+//		4. 사용자로부터 입력받은 문자열이 "Hello"인지 아닌지 판별하는 프로그램을 작성하세요.
+//		Scanner scanner = new Scanner(System.in);
+//		System.err.println("문자열을 입력하세요");
+//		String str = scanner.nextLine();
+//		String str2 = "Hello";
+//		if (str.equals(str2)) {
+//			System.out.println("입력받은 문자열은 Hello입니다");
+//		}else {
+//			System.out.println("입력받은 문자열은 Hello가 아닙니다.");
+//		}
+	
+//
+//		5. 삼항연산자를 이용하여 사용자가 입력한 정수가 홀수면 "홀수" 짝수면 "짝수"라고 출력하세요.
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("정수를 입력하세요. ");
+//		int a = scanner.nextInt(); 
+//		String result = (a % 2 == 0) ? "짝수" : "홀수";
+//		System.out.println(a + "는" + result +"입니다");
+//
+//		6. 삼항연산자의 중첩을 이용하여 사용자가 입력한 정수가 100보다 크면 "100보다 큼" 작으면 "100보다 작음" 같으면 "100"을 출력하세요.
+//
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("정수를 입력하세요. ");
+//		int a = scanner.nextInt(); 
+//		String result = (a > 100) ? "100보다 큼" : "100보다 작음";
+//		System.out.println(a + "는" + result);
+		
+		
+//		7. 사용자가 입력한 두 실수의 덧셈, 뺄셈, 곱셈, 나눗셈, 나머지 연산의 결과를 출력하세요.
+		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("첫번째 정수를 입력하세요. ");
+//        double a = scanner.nextInt(); 
+//        System.out.println("두번째 정수를 입력하세요. ");
+//        double b = scanner.nextInt(); 
+//        double sum = a + b;
+//        double sub = a - b;
+//        double mul = a * b;
+//        double div = a / b;
+//        
+//        
+//        System.out.println("두 수의 + 값:" + sum);
+//        System.out.println("두 수의 - 값:" + sub);
+//        System.out.println("두 수의 * 값:" + mul);
+//        System.out.println("두 수의 / 값:" + div);
+//
+//		8. 사용자가 입력한 하나의 문자열과 하나의 정수로 "입력한 문자열 xxxxx이고, 입력한 숫자는 xxxxxx입니다." 라고 출력하세요.	
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("정수를 입력하세요. ");
+//		int a = scanner.nextInt(); 
+//		scanner.nextLine();
+//		System.out.println("문자열을 입력하세요");
+//		String str = scanner.nextLine();
+//		
+//		
+//		System.out.println("입력한 문자열은" + str + "입력한 숫자는" + a);
+		
+//		1. 사용자가 입력한 두 정수 중에서 큰 정수를 출력하세요.
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("첫번째 정수를 입력하세요. ");
+//        int a = scanner.nextInt(); 
+//        System.out.println("두번째 정수를 입력하세요. ");
+//        int b = scanner.nextInt(); 
+//        if (a > b) {
+//        	System.out.println(a);
+//        }else {
+//        	System.out.println(b);
+//        }
+		
+		
+//
+//		2. 사용자가 입력한 정수가 양수면 "양수" 음수면 "음수"라고 출력하세요.
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("첫번째 정수를 입력하세요. ");
+//        int a = scanner.nextInt(); 
+//        
+//        if (a >=0 ) {
+//        	System.out.println("양수");
+//        }else {
+//        	System.out.println("음수");
+//        }
+		
+		
+		
+//
+//		3. 사용자가 입력한 문자열이 "Java"인 경우 "좋아하는 언어입니다."를 출력하고, "Python"인 경우 "공부중인 언어입니다."를 출력하고, 그렇지 않은 경우 "다른 언어를 공부해보세 
+//		     요."를 출력하세요.(switch~case~default 사용)
+//		Scanner scanner = new Scanner(System.in);
+//		System.err.println("문자열을 입력하세요");
+//		String str = scanner.nextLine();
+//		
+//		switch(str) 
+//		{
+//		case "Java":
+//			System.out.println("좋아하는 언어입니다.");
+//			break;
+//		case "Python":
+//			System.out.println("공부중인 언어입니다.");	
+//			break;
+//		default :	
+//			System.out.println("다른 언어를 공부해보세요.");
+//			break;
+//		}
+		
+		
+//
+//		4. 사용자가 입력한 숫자가 3의 배수면 "3의 배수입니다."라고 출력하고 3의 배수가 아니면 "3의 배수가 아닙니다."를 출력하세요.(switch~case~default 사용)
+//
+//		Scanner scanner = new Scanner(System.in);
+//		System.err.println("숫자를 입력하세요");
+//		int a = scanner.nextInt();
+//		switch(a % 3) 
+//		{
+//		case 0:
+//			System.out.println("3의 배수입니다.");
+//			break;
+//		default :	
+//			System.out.println("3의 배수가 아닙니다.");
+//			break;
+//		}
+		
+		
+//		5. 사용자가 입력한 숫자가 홀수인 경우 "홀수입니다."를 출력하고, 짝수인 경우 "짝수입니다."를 출력하는 프로그램을 작성하세요.
+		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("정수를 입력하세요. ");
+//		int a = scanner.nextInt();
+//		switch(a % 2) 
+//		{
+//		case 0:
+//			System.out.println("짝수입니다.");
+//			break;
+//		default :	
+//			System.out.println("홀수입니다.");
+//			break;
+//		}
+		
+		
+//
+//		6. 사용자가 입력한 5개의 정수중 가장 큰 수를 출력하세요.
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("첫번쨰 정수를 입력하세요. ");
+//		int a = scanner.nextInt();
+//		System.out.println("두번쨰 정수를 입력하세요. ");
+//		int b = scanner.nextInt();
+//		System.out.println("세번쨰 정수를 입력하세요. ");
+//		int c = scanner.nextInt();
+//		System.out.println("네번쨰 정수를 입력하세요. ");
+//		int d = scanner.nextInt();
+//		System.out.println("다섯번쨰 정수를 입력하세요. ");
+//		int e = scanner.nextInt();
+//		int max = a;
+//		if (b > max) {
+//			max = b;
+//		}
+//		if (c > max) {
+//			max = c;
+//		}
+//		if (d > max) {
+//			max = d;
+//		}
+//		if (e > max) {
+//			max = e;
+//		}
+//		
+//		System.out.println("입력값 중 최대값:" + max);
+		
+	
+		
+//
+//		7. 사용자가 입력한 문자열이 "Yes"인 경우 "예"를 출력하고, "No"인 경우 "아니오"를 출력하고, 그 외의 경우에는 "잘못 입력하셨습니다."를 출력하세요.
+//
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("문자열을 입력하세요");
+//		String str = scanner.nextLine();
+//		
+//		switch(str) 
+//		{
+//		case "Yes":
+//			System.out.println("예");
+//			break;
+//		case "No":
+//			System.out.println("아니오");	
+//			break;
+//		default :	
+//			System.out.println("잘못 입력하셨습니다.");
+//			break;
+//		}
+		
+	
+//		8. 사용자가 입력한 두 정수를 비교하여 같으면 "두 수가 같습니다." 첫 번째 수가 더 크면 "첫 번째 수가 더 큽니다." 두 번째 수가 더 크면 "두 번째 수가 더 큽니다."를 출력하세요.
+//
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("첫번째 정수를 입력하세요. ");
+//        int a = scanner.nextInt(); 
+//        System.out.println("두번째 정수를 입력하세요. ");
+//        int b = scanner.nextInt(); 
+//        
+//        if (a == b) {
+//            System.out.println("두 수가 같습니다.");
+//        } else if (a > b) {
+//            System.out.println("첫 번째 수가 더 큽니다.");
+//        } else {
+//            System.out.println("두 번째 수가 더 큽니다.");
+//        }
+		
+		
+//		9. 사용자가 입력한 세 정수가 모두 짝수인 경우 "모두 짝수입니다." 모두 홀수인 경우 "모두 홀수입니다." 그 외의 경우 "짝수 : xxx개, 홀수 : xxx개 입니다."를 출력하세요.
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("첫번째 정수를 입력하세요. ");
+        int a = scanner.nextInt(); 
+        System.out.println("두번째 정수를 입력하세요. ");
+        int b = scanner.nextInt(); 
+        System.out.println("세번째 정수를 입력하세요. ");
+        int c = scanner.nextInt(); 
+		
+        int evenCount = 0;
+        int oddCount = 0;
+        
+        if(a % 2 == 0) evenCount++; else oddCount++;
+        if(b % 2 == 0) evenCount++; else oddCount++;
+        if(c % 2 == 0) evenCount++; else oddCount++;
+        
+        if (evenCount == 3) {
+            System.out.println("모두 짝수입니다.");
+        } else if (oddCount == 3) {
+            System.out.println("모두 홀수입니다.");
+        } else {
+            System.out.println("짝수 : " + evenCount + "개, 홀수 : " + oddCount + "개 입니다.");
+        }
+		
+		
 	}
 }
