37925d45696341a005b0229bcb2f638d9f6f5548
chunghyun <arisu0304@naver.com>
2024-05-29 20:14:12 +0900

    240529 디비 숙제

--- LeeChungHyun - homework/백준/17143 낚시왕.cpp
+++ LeeChungHyun - homework/백준/17143 낚시왕.cpp
@@ -35,19 +35,20 @@
 //낚시왕이 잡은 상어 크기의 합을 출력한다.
 
 #include<iostream>
+#include<set>
 using namespace std;
 
 const int MAX = 100;
 
 // int d에 맞춰서 설정
-int dx[4] = { 0 ,0 ,1 ,-1 };
-int dy[4] = { 1, -1, 0, 0 };
+int dr[5] = { 0, -1 , 1 , 0 , 0 };
+int dc[5] = { 0, 0 , 0 , 1 , -1 };
 
 // 상어 구조체
 struct Shark {
-	// 좌표
-	int x;
-	int y;
+	// 좌표 행, 렬
+	int r;
+	int c;
 	// 속도
 	int s;
 	// 방향 1,2,3,4 -> 상하우좌
@@ -57,72 +58,154 @@ struct Shark {
 };
 
 // 상어 배열
-Shark shark[MAX * MAX];
+Shark shark[MAX * MAX + 1];
 
 // 상어 죽었는지 체크 죽으면 false
-bool is_alive[MAX * MAX];
+bool is_alive[MAX * MAX + 1];
 
-// 낚시장 크기
-int M;
-int N;
+// 낚시장 크기 행, 렬
+int R;
+int C;
 
 // 상어 마리수
-int num;
+int M;
 
 // 상어가 있는지 체크 있으면 true 하려고 했지만 정확히 몇번 상어가 있는지도 필요
-int is_shark[MAX + 1][MAX + 1];
+// 이동 후 한 칸에 여러마리 있을 수 있으므로 각 좌표에 하나씩 해시셋이 붙어있는게 편함
+// 어차피 상어 들어가는 순서는 상관 없고 제거가 용이한 자료구조가 더 좋음
 
+set<int> is_shark[MAX + 1][MAX + 1];
+
+// 잡은 총 생선 크기
+int fish;
+
+int pos = 1;
+
+void input();
+
+// 각 상어가 한번 움직이는 메소드
+void next_shark(int i);
+
+// 모든 상어가 움직이고 서로 잡아먹는 메소드
 void move_shark();
 
+// 낚시꾼이 상어를 잡는 메소드
+void fishing();
+
 void input() {
-	cin >> M >> N >> num;
-	for (int i = 0; i < num; i++) {
-		cin >> shark[i].x >> shark[i].y >> shark[i].s >> shark[i].d >> shark[i].z;
-		is_shark[shark[i].x][shark[i].y] = true;
+	cin >> R >> C >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> shark[i].r >> shark[i].c >> shark[i].s >> shark[i].d >> shark[i].z;
+		// 상어가 하나 태어남
+		is_alive[i] = true;
+		// 각 상어의 좌표에 상어의 번호를 저장
+		is_shark[shark[i].r][shark[i].c].insert(i);
 	}
 }
 
-// i번째 상어 이동
+// i번째 상어의 이동
 void next_shark(int i) {
 
-	if (shark[i].d == 0 || shark[i].d == 1) {
-		int move = (shark[i].s * dy[shark[i].d]);
-		while (!(1 <= shark[i].y + move && shark[i].y + move <= N)) {
-			shark[i].y = ( -(shark[i].y + move) ) % N;
-			if (shark[i].y == 0) {
-				shark[i].y = N;
+	// 움직임이 행방향
+	if (shark[i].d == 1 || shark[i].d == 2) {
+		// 어차피 2R-2를 주기로 방향, 위치가 동일
+		int speed = (shark[i].s) % (2 * R - 2);
+		int move = 0;
+		// 나머지 취한뒤 1씩 증가시킴
+		while (move < speed) {
+			// 움직여도 벽과 안 만나면 그냥 그 자리
+			if (1 <= shark[i].r + dr[shark[i].d] && shark[i].r + dr[shark[i].d] <= R) {
+				shark[i].r = shark[i].r + dr[shark[i].d];
 			}
-			move -= N;
-			shark[i].d = 1 - shark[i].d;
+			// 벽을 만나면 방향도 변해야함
+			else {
+				shark[i].d = 3 - shark[i].d;
+				shark[i].r = shark[i].r + dr[shark[i].d];
+			}
+			move++;
 		}
 	}
+	// 위와 동일
 	else {
-		int move = (shark[i].s * dx[shark[i].d]);
-		while (!(1 <= shark[i].x + move && shark[i].x + move <= N)) {
-			shark[i].x = (-(shark[i].x + move)) % M;
-			if (shark[i].x == 0) {
-				shark[i].x = M;
+		int speed = (shark[i].s) % (2 * C - 2);
+		int move = 0;
+		while (move < speed) {
+			if (1 <= shark[i].c + dc[shark[i].d] && shark[i].c + dc[shark[i].d] <= C) {
+				shark[i].c = shark[i].c + dc[shark[i].d];
 			}
-			move -= M;
-			shark[i].d = 7 - shark[i].d;
+			else {
+				shark[i].d = 7 - shark[i].d;
+				shark[i].c = shark[i].c + dc[shark[i].d];
+			}
+			move++;
+		}
 	}
 }
 
+// 전체 상어의 이동 후 잡아먹히는 것까지 갱신
 void move_shark() {
-	for (int i = 0; i < num; i++) {
+	//문제는 잡아먹히는 것이 동시적인 사건이 아니고 모든 상어가 이동한 뒤에 발생
+	for (int i = 0; i < M; i++) {
+		// 죽어있는 건 움직이지 않음
 		if (is_alive[i]) {
-			is_shark[shark[i].x][shark[i].y] = false;
+			// 움직이면 거기 있던 상어 제거
+			is_shark[shark[i].r][shark[i].c].erase(i);
 			next_shark(i);
-			if (is_shark[shark[i].x][shark[i].y]) {
-
+			// 움직이고 난 뒤 상어 추가
+			is_shark[shark[i].r][shark[i].c].insert(i);
+		}
+	}
+	// 이제 각 좌표에 할당된 해시셋의 크기가 2 이상이면 제일 사이즈 큰 상어만 남기고 나머지는 죽여야함
+	for (int i = 0; i < M; i++) {
+		// 살아있는 것만
+		if (is_alive[i]) {
+			// 한 칸에 2개 이상 살아있는 상어가 있을 때
+			if (is_shark[shark[i].r][shark[i].c].size() >= 2) {
+				int big_shark = i;
+				// 먼저 최대 사이즈 상어 찾기
+				for (int j : is_shark[shark[i].r][shark[i].c]) {
+					if (shark[j].z > shark[big_shark].z) {
+						big_shark = j;
+					}
+					is_alive[j] = false;
+				}
+				is_shark[shark[i].r][shark[i].c].clear();
+				is_alive[big_shark] = true;
+				is_shark[shark[i].r][shark[i].c].insert(big_shark);
 			}
 		}
 	}
 }
 
 void fishing() {
-	// 남자 이동후 낚시
-	// 상어 이동
+
+	while (pos <= C) {
+		
+		// 낚시
+		for (int i = 1; i <= R; i++) {
+			// 만약 낚시꾼이 있는 열에 상어가 하나라도 있다면
+			if (!is_shark[i][pos].empty()) {
+				// 어차피 각 칸에는 1마리의 상어만 있으므로 최초를 꺼내는 것과 동일
+				int num_shark = *is_shark[i][pos].begin();
+				// 잡은 상어 크기 추가
+				fish += shark[num_shark].z;
+				// 상어 죽임
+				is_alive[num_shark] = false;
+				// 상어 잡았으므로 비움
+				is_shark[i][pos].clear();
+				// 제일 가까운 상어만 잡음
+				break;
+			}
+		}
+		
+		// 상어 이동
+		move_shark();
+
+		
+
+		// 낚시꾼 이동
+		pos++;
+	}
 }
 
 int main() {
@@ -134,6 +217,9 @@ int main() {
 	freopen_s(&filestream, "input.txt", "r", stdin);
 
 	input();
+	fishing();
+	cout << fish;
+
 	return 0;
 
 }
